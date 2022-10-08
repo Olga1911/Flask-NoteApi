37b58c16408b355032313cf08387b61f53e8b12f
Olga1911 <olga.sukhovich@gmail.com>
2022-10-08 21:05:26 +0300

    My commit after lesson

--- api/resources/user.py
+++ api/resources/user.py
@@ -1,42 +1,66 @@
 from api import Resource, abort, reqparse, auth
 from api.models.user import UserModel
-from api.schemas.user import user_schema, users_schema
+from flask_apispec.views import MethodResource
+from flask_apispec import marshal_with, use_kwargs, doc
+from api.schemas.user import UserSchema, UserRequestSchema
+#from webargs import fields
 
+#parameters:
+#1. path
+#2. body
+#3. query params
 
-class UserResource(Resource):
+@doc(tags=['Users'])
+class UserResource(MethodResource):
+    @doc(summary="Get user by id", description="Returns user")
+    @doc(responses={404: {"description": 'User not found'}})
+    @marshal_with(UserSchema, code=200)
     def get(self, user_id):
         user = UserModel.query.get(user_id)
         if user is None:
-            abort(403, error=f"User with id={user_id} not found")
-        return user_schema.dump(user), 200
+            abort(404, error=f"User with id={user_id} not found")
+        return user, 200
 
     @auth.login_required(role="admin")
-    def put(self, user_id):
-        parser = reqparse.RequestParser()
-        parser.add_argument("username", required=True)
-        user_data = parser.parse_args()
+    @doc(description='Edit user by id')
+    @marshal_with(UserSchema)
+    @use_kwargs(UserRequestSchema, location='json')
+    #@use_kwargs({"username": fields.Str()})
+    @doc(security=[{"basicAuth": []}])
+    def put(self, user_id, **kwargs):
         user = UserModel.query.get(user_id)
-        user.username = user_data["username"]
+        user.username = kwargs["username"]
         user.save()
-        return user_schema.dump(user), 200
+        return user, 200
 
     @auth.login_required
+    @doc(summary='Delete a user')
     def delete(self, user_id):
-        raise NotImplemented  # не реализовано!
+        user = UserModel.query.get(user_id)
+        if user is None:
+            return {"Error": f"Author id={user_id} not found"}, 404
+        user.delete()
+        return "", 204
+        # raise NotImplemented  # не реализовано!
 
-
-class UsersListResource(Resource):
+@doc(tags=['Users'])
+class UsersListResource(MethodResource):
+    @marshal_with(UserSchema(many=True), code=200)
+    @doc(summary='Get ALL users')
     def get(self):
         users = UserModel.query.all()
-        return users_schema.dump(users), 200
+        return users, 200
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("username", required=True)
-        parser.add_argument("password", required=True)
-        user_data = parser.parse_args()
-        user = UserModel(**user_data)
+    @doc(summary="Create new User", description="Подробное описание метода POST")
+    @use_kwargs(UserRequestSchema, location='json')
+    @marshal_with(UserSchema, code=201)
+    def post(self, **kwargs):
+        user = UserModel(**kwargs)
         user.save()
         if not user.id:
             abort(400, error=f"User with username:{user.username} already exist")
-        return user_schema.dump(user), 201
+        return user, 201
+
+
+# Сериализация: obj --> dict --> str
+# Десериализация: str --> dict --> obj
