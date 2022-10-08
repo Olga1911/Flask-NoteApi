ffded58db0ecc6ca731c1df7c87ba33058d6ee3b
Olga1911 <olga.sukhovich@gmail.com>
2022-10-08 21:04:04 +0300

    My commit after lesson

--- api/models/user.py
+++ api/models/user.py
@@ -2,8 +2,8 @@ from api import db, Config, ma
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import URLSafeTimedSerializer as Serializer
 from itsdangerous import BadSignature, SignatureExpired
-
 from sqlalchemy.exc import IntegrityError
+from itsdangerous import URLSafeSerializer
 
 
 class UserModel(db.Model):
@@ -14,9 +14,13 @@ class UserModel(db.Model):
     is_staff = db.Column(db.Boolean(), default=False, server_default="false", nullable=False)
     role = db.Column(db.String(32), nullable=False, server_default="simple_user", default="simple_user")
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, role="simple_user"):
         self.username = username
+        self.role = role
         self.hash_password(password)
+
+    def get_roles(self):
+        return [self.role]
 
     def hash_password(self, password):
         self.password_hash = pwd_context.hash(password)
@@ -24,8 +28,8 @@ class UserModel(db.Model):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(Config.SECRET_KEY, expires_in=expiration)
+    def generate_auth_token(self):
+        s = URLSafeSerializer(Config.SECRET_KEY)
         return s.dumps({'id': self.id})
 
     def save(self):
@@ -44,8 +48,6 @@ class UserModel(db.Model):
         s = Serializer(Config.SECRET_KEY)
         try:
             data = s.loads(token)
-        except SignatureExpired:
-            return None  # valid token, but expired
         except BadSignature:
             return None  # invalid token
         user = UserModel.query.get(data['id'])
