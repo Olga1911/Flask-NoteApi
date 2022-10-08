4e646621b57744161f1e99dfe9b5aa18eee1e4b0
Olga1911 <olga.sukhovich@gmail.com>
2022-10-08 21:07:07 +0300

    My commit after lesson

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 from pathlib import Path
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
 
 BASE_DIR = Path(__file__).parent
+security_definitions = {
+   "basicAuth": {
+       "type": "basic"
+   }
+}
 
 
 class Config:
+    PATH_TO_FIXTURES = BASE_DIR / "fixtures"
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR / 'main.db'}"
+    TEST_DATABASE = f"sqlite:///:memory:"
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Зачем эта настройка: https://flask-sqlalchemy-russian.readthedocs.io/ru/latest/config.html#id2
     DEBUG = True
     PORT = 5000
@@ -12,3 +21,13 @@ class Config:
     RESTFUL_JSON = {
         'ensure_ascii': False,
     }
+    APISPEC_SPEC = APISpec(
+        title='Notes Project',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        securityDefinitions=security_definitions,
+        security=[],
+        openapi_version='2.0.0'
+    )
+    APISPEC_SWAGGER_URL = '/swagger'  # URI API Doc JSON
+    APISPEC_SWAGGER_UI_URL = '/swagger-ui'  # URI UI of API Doc
